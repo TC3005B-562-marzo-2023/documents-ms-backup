7699b71857586d093e01a722fd546a7e5c887191
Manu Capa <manu@Manus-MacBook-Pro.local>
2023-04-10 16:56:07 -0600

    first-commit

---
+++ src/main/java/com/driveai/documentsms/DocumentsMsApplication.java
@@ -0,0 +1,13 @@
+package com.driveai.documentsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentsMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/driveai/documentsms/DocumentsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driveai.documentsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
